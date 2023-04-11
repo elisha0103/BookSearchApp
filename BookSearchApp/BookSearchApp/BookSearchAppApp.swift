d058796cb493b42a0843855e6d9334ed3314db59
Taeyoung Jin <elisha0103@naver.com>
2023-04-11 11:02:33 +0900

    Create Project
    최초 프로젝트 생성

---
+++ BookSearchApp/BookSearchApp/BookSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchAppApp.swift
+//  BookSearchApp
+//
+//  Created by 진태영 on 2023/04/11.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
